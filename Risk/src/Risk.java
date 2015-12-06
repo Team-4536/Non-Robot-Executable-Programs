065a0610aafb7f3005af0b0f0352a8595c0e07d0
Paul <paulmacgregor98@gmail.com>
2015-12-05 21:44:04 -0600

    First Commit

---
+++ Risk/src/Risk.java
@@ -0,0 +1,216 @@
+
+import java.lang.Math;
+import java.util.Scanner;
+
+public class Risk {
+	
+	public static int getMax(int a, int b) {
+		
+		if (b > a) {
+			
+			return b;
+		}
+		else {
+			
+			return a;
+		}
+	}
+
+	public static int getMax(int a, int b, int c) {
+		
+		return getMax(getMax(a, b), c);
+	}
+	
+	public static int getMax(int [] arr) {
+		
+		int max = arr [0];
+		
+		for (int i = 0; i < arr.length; i++) {
+			
+			if (arr[i] > max) {
+				
+				max = arr[i];
+			}
+		}
+		
+		return max;
+	}
+	
+	public static int secondMax(int [] arr) {
+		
+		int max1 = getMax(arr);
+		
+		int max2 = 0;
+		
+		int c = 0;
+		
+		//Test if there are two or more equal maximums
+		for (int i = 0; i < arr.length; i++) {
+			
+			if (arr[i] == max1) {
+				
+				c++;
+			}
+		}
+		
+		if (c >= 2) {
+			
+			return max1;
+		}
+		else {
+			
+			for (int i = 0; i < arr.length; i++) {
+				
+				if (arr[i] > max2 && arr[i] < max1) {
+					
+					max2 = arr[i];
+				}
+			}
+			
+			return max2;
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		boolean fightToDeath;
+		
+		Scanner userInput = new Scanner(System.in);
+		
+		boolean repeat = true;
+		
+		//Initialize Deathmatch
+		System.out.println("Fight to the death? ");
+		fightToDeath = userInput.nextBoolean();
+		
+		//Initialize Attackers and Defenders
+		System.out.println("Please enter the number of attacking troops "
+							+ "and defending troops: ");
+		int attackers = userInput.nextInt();
+		int defenders = userInput.nextInt();
+		
+		int attackersLost = 0;
+		int defendersLost = 0;
+		
+		//Loop Battle!
+		while (repeat) {
+			
+			//Protect number of attackers and defenders from becoming less than zero.
+			if (attackers <= 0 || defenders <= 0) {
+				
+				break;
+			}
+			
+			//Initialize attacker rolls based on number of attackers
+			int [] attackerRolls;
+			
+			if (attackers >= 3) {
+				
+				attackerRolls = new int[3];
+			}
+			else if (attackers == 2) {
+				
+				attackerRolls = new int[2];
+			}
+			else {
+				
+				attackerRolls = new int[1];
+			}
+			
+			for (int i = 0; i < attackerRolls.length; i++) {
+				
+				attackerRolls[i] = (int)(Math.random()*6 + 1);
+			}
+			
+			//DEBUG
+			for (int i = 0; i < attackerRolls.length; i++) {
+				
+				System.out.println("Attacker Roll #" + i + ": " + attackerRolls[i]);
+			}
+			
+			//Initialize Defender Rolls based on number of defenders
+			int [] defenderRolls;
+			
+			if (defenders >= 2) {
+				
+				defenderRolls = new int [2];
+			}
+			else {
+				
+				defenderRolls = new int [1];
+			}
+			
+			for (int i = 0; i < defenderRolls.length; i++) {
+				
+				defenderRolls[i] = ((int)(Math.random()*6)) + 1;
+			}
+			
+			//DEBUG
+			for (int i = 0; i < defenderRolls.length; i++) {
+				
+				System.out.println("Defender Roll #" + i + ": " + defenderRolls[i]);
+			}
+			
+			//Compare Rolls
+			//Case: Highest of both
+			if (attackers >= 2 && defenders >= 2) {
+				
+				if (getMax(attackerRolls) > getMax(defenderRolls)) {
+					
+					defenders--;
+					defendersLost++;
+				}
+				else {
+					
+					attackers--;
+					attackersLost++;
+				}
+				
+				if (secondMax(attackerRolls) > secondMax(defenderRolls)) {
+					
+					defenders--;
+					defendersLost++;
+				}
+				else {
+					
+					attackers--;
+					attackersLost++;
+				}
+			}
+			else {
+				
+				if (getMax(attackerRolls) > getMax(defenderRolls)) {
+					
+					defenders--;
+					defendersLost++;
+				}
+				else {
+					
+					attackers--;
+					attackersLost++;
+				}
+			}
+			
+			System.out.println("\n\nBattle Statistics: ");
+			System.out.println("Attackers Left: " + attackers);
+			System.out.println("Defenders Left: " + defenders);
+			System.out.println("Attackers Lost: " + attackersLost);
+			System.out.println("Defenders Lost: " + defendersLost);
+			
+			if (!fightToDeath) {
+				
+				//User End Simulation
+				System.out.println("Continue (y/n)? ");
+				String input = userInput.next();
+				
+				if (input.equals("n")) {
+					
+					repeat = false;
+					break;
+				}
+			}
+			
+		}
+	}
+
+}
